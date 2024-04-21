8259f8cd0f7b579baf8f73c758b60984168b083b
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-04-21 18:47:31 +0500

    models: post+, get+, patch+

--- src/pages/models/index.tsx
+++ src/pages/models/index.tsx
@@ -77,6 +77,7 @@ const index = () => {
             type="search"
             className="form-control"
             placeholder="Search Model..."
+            
           />
           <button className="btn btn-primary rounded-r-none" type="button">
             <i className="fa-solid fa-magnifying-glass"></i>
